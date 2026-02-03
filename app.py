faed7a83723c0d585ba57ac4a159a6a000095910
Melissa Olivares <123840619+Molivaresg@users.noreply.github.com>
2026-02-02 21:51:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ Eres un asistente para proveedores. Responde basándote en esto:
 # 3. Configurar IA (Usando Secrets)
 try:
     genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
-    model = genai.GenerativeModel('models/gemini-1.5-flash-latest')
+    model = genai.GenerativeModel(model_name="models/gemini-1.5-flash")
 except Exception as e:
     st.error("Error con la API Key. Verifica los Secrets en Streamlit.")
 
